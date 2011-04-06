bb8e79d9d37ed642a6911ae3a93b81992a2a2f32
stefan.faridani@gmail.com <stefan.faridani@gmail.com@a96d8735-e8e0-b988-51c3-20b6b8abdaed>
2011-04-06 23:58:38 +0000

    Implemented a tile system with walls represented by an array in PlayState.cs with 1 representing a wall. No collisions yet.

--- trunk/Incendia/Incendia/Character.cs
+++ trunk/Incendia/Incendia/Character.cs
@@ -17,14 +17,28 @@ namespace Incendia
         {
         }
 
-        public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
+        public override void Update(Microsoft.Xna.Framework.GameTime gameTime, PlayState map) 
         {
-            base.Update(gameTime);
+            base.Update(gameTime, map);
+            KeepInBounds(map);
         }
 
         public override void Draw(Microsoft.Xna.Framework.Graphics.SpriteBatch batch)
         {
             base.Draw(batch);
+        }
+
+        private void KeepInBounds(PlayState map)
+        {
+            //Place the character within the bounds of the world
+            if (Position.X < 0)
+                SetPositionX(0);
+            else if (Position.X + Visual.Width > map.WorldLimits.X)
+                SetPositionX(map.WorldLimits.X - Visual.Width);
+            if (Position.Y < 0)
+                SetPositionY(0);
+            else if (Position.Y + Visual.Height > map.WorldLimits.Y)
+                SetPositionY(map.WorldLimits.Y - Visual.Height);
         }
 
 
--- trunk/Incendia/Incendia/Sprite.cs
+++ trunk/Incendia/Incendia/Sprite.cs
@@ -14,20 +14,20 @@ namespace Incendia
     class Sprite
     {
 
-        protected float _rotation;
-        protected float _scale;
-        protected Vector2 _position;
+        protected float _rotation; // In radians
+        protected float _scale; //In factors
+        protected Vector2 _position; //Location of upper-right corner in tiles
         protected float _rotationSpeed; //In radians per second 
         protected float _scaleSpeed;//In factors per second
-        protected Vector2 _velocity; //In pixils per second
+        protected Vector2 _velocity; //In tiles per second
         protected Animation _defaultAnimation;
         protected bool _dead;//Don't draw or update if you're dead
-        protected SpriteEffects _effects;
+        protected SpriteEffects _effects; //Flipping horizontally, vertically or not at all
         protected double _lifeTime;//In seconds
 
         //Return a rectangle based on the current position and the size of the current default frame
         //May be overridden by a parent class
-        public virtual Rectangle Visual { get { return new Rectangle((int)_position.X, (int)_position.Y, _defaultAnimation.GetFrame().Rectangle.Width, _defaultAnimation.GetFrame().Rectangle.Height); } }
+        public virtual Rectangle Visual { get { return new Rectangle((int)_position.X, (int)_position.Y, (int)((float)_defaultAnimation.GetFrame().Rectangle.Width / Global.PixelsPerTile), (int)((float)_defaultAnimation.GetFrame().Rectangle.Height / Global.PixelsPerTile)); } }
 
         public float Rotation { get { return _rotation; } set { _rotation = value; } }
         public float Scale { get { return _scale; } set { _scale = value; } }
@@ -35,7 +35,8 @@ namespace Incendia
         public float ScaleSpeed { get { return _scaleSpeed; } set { _scaleSpeed = value; } }
         public Vector2 Velocity { get { return _velocity; } }
         public bool Dead { get { return _dead || _scale <= 0; } set { _dead = value; } }
-        public Vector2 Position { get { return _position; } }
+        public Vector2 Position { get { return _position / Global.PixelsPerTile; } } //Location of upper-right corner in tiles
+        public Vector2 PositionCenter { get { return new Vector2(_position.X + Visual.Width, _position.Y + Visual.Height) / Global.PixelsPerTile; } } //Location of center in tiles
 
 
         public Sprite(Vector2 position, Animation defaultAnimation, double lifeTime)
@@ -48,13 +49,13 @@ namespace Incendia
             _effects = SpriteEffects.None;
         }
 
-        public virtual void Update(GameTime gameTime)
+        public virtual void Update(GameTime gameTime, PlayState map)
         {
             if (Dead)
                 return;
             _rotation += _rotationSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
             _scale += _scaleSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-            _position += _velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            _position += _velocity * (float)gameTime.ElapsedGameTime.TotalSeconds * Global.PixelsPerTile;
 
             if (_lifeTime >= 0)
             {
@@ -68,10 +69,11 @@ namespace Incendia
         {
             if (Dead)
                 return;
-            _defaultAnimation.GetFrame().Draw(_position, batch, _rotation, _scale, _effects);
+            _defaultAnimation.GetFrame().Draw(new Vector2(_position.X + Visual.Width * (Global.PixelsPerTile / 2), _position.Y + Visual.Height * (Global.PixelsPerTile / 2)), batch, _rotation, _scale, _effects);
         }
 
 
+        //Field Accessors
         public void SetVelocityX(float x)
         {
             _velocity.X = x;
@@ -80,6 +82,15 @@ namespace Incendia
         public void SetVelocityY(float y)
         {
             _velocity.Y = y;
+        }
+        public void SetPositionX(float x)
+        {
+            _position.X = x * Global.PixelsPerTile;
+        }
+
+        public void SetPositionY(float y)
+        {
+            _position.Y = y * Global.PixelsPerTile;
         }
 
     }
