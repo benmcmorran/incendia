c682f74416642dfca9b39368cc6d2adcb96aaa48
ben.mcmorran <ben.mcmorran@a96d8735-e8e0-b988-51c3-20b6b8abdaed>
2011-04-08 03:22:47 +0000

    Camera now tracks player.

--- trunk/Incendia/Incendia/PlayState.cs
+++ trunk/Incendia/Incendia/PlayState.cs
@@ -14,9 +14,10 @@ namespace Incendia
         Character _player;
         public Vector2 WorldLimits { get; set; }
         uint[,] grid;
+        Camera2D camera;
+        Viewport viewport;
 
-
-        public PlayState(uint horizontalTiles, uint verticalTiles)
+        public PlayState(uint horizontalTiles, uint verticalTiles, Viewport viewport)
         {
             _player = Generator.PlayerSprite(new Vector2(0, 0));
             WorldLimits = new Vector2((float)horizontalTiles, (float)verticalTiles);
@@ -28,16 +29,22 @@ namespace Incendia
             grid[1, 0] = 1;
             grid[19, 14] = 1;
             grid[18, 13] = 1;
+
+            camera = new Camera2D();
+            this.viewport = viewport;
         }
 
         public void Update(GameTime gameTime)
         {
             TakeInput();
             _player.Update(gameTime, this);
+            camera.Location = _player.Position * Global.PixelsPerTile;
         }
 
         public void Draw(SpriteBatch batch)
         {
+            batch.Begin(SpriteSortMode.Deferred, null, null, null, null, null, camera.ViewTransformationMatrix(viewport));
+            
             _player.Draw(batch);
 
             //Draw what is on the grid
@@ -49,12 +56,14 @@ namespace Incendia
                         batch.Draw(Global.Textures["Wall"], new Vector2(x,y) * Global.PixelsPerTile, null, Color.White, 0, Vector2.Zero, Global.PixelsPerTile / Global.Textures["Wall"].Width, SpriteEffects.None, 0);
                 }
             }
+
+            batch.End();
         }
 
         void TakeInput()
         {
             if (Input.DistanceToMouse(_player.PositionCenter * Global.PixelsPerTile) > Global.PixelsPerTile * _player.Visual.Height / 2)
-                _player.Rotation = Input.AngleToMouse(_player.PositionCenter * Global.PixelsPerTile);
+                _player.Rotation = Input.AngleToMouse(Vector2.Transform(_player.PositionCenter * Global.PixelsPerTile, camera.ViewTransformationMatrix(viewport)));
             if (Input.KeyHeld(Keys.W))
                 _player.SetVelocityY(-3);
             else if (Input.KeyHeld(Keys.S))
