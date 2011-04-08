b9adb776fe6656ceb0298aac9be70b3a7e1970e3
ben.mcmorran <ben.mcmorran@a96d8735-e8e0-b988-51c3-20b6b8abdaed>
2011-04-08 02:48:36 +0000

    Added particle manipulators.

---
+++ Incendia/Incendia/IParticleManipulator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Incendia
+{
+    interface IParticleManipulator
+    {
+        void ManipulateParticle(Particle particle);
+    }
+}
