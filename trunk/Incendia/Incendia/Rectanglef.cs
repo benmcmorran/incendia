346c9317886881bdedcbf35cfbd9f9e9e7181170
stefan.faridani@gmail.com <stefan.faridani@gmail.com@a96d8735-e8e0-b988-51c3-20b6b8abdaed>
2011-04-16 22:07:14 +0000

    Added missing Rectanglef class.

---
+++ trunk/Incendia/Incendia/Rectanglef.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Incendia
+{
+    struct Rectanglef
+    {
+        //In tiles
+        public float X;
+        public float Y;
+        public float Width;
+        public float Height;
+
+        public Rectanglef(float x, float y, float width, float height)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+        }
+    }
+}
