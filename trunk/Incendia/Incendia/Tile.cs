0e71ead1dab21245261b8ac79f53687d0e6a7332
stefan.faridani@gmail.com <stefan.faridani@gmail.com@a96d8735-e8e0-b988-51c3-20b6b8abdaed>
2011-04-23 21:11:06 +0000

    Changed grid in Playstate to a set of tile objects with their own state.

---
+++ trunk/Incendia/Incendia/Tile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Incendia
+{
+    class Tile
+    {
+        public float Flammability { get; private set; }
+        public int Material { get; private set; }
+        public FireState State { get; private set; }
+        public string _texture;
+        public bool _solid;
+        public bool Solid { get { return _solid || State == FireState.Burnt; } }
+        public string Texture
+        {
+            get
+            {
+                if (State == FireState.Burnt)
+                    return _texture + "b";
+                else
+                    return _texture;
+            }
+        }
+
+        public Tile(float flammability, int material, FireState state, string texture, bool solid)
+        {
+            Flammability = flammability;
+            Material = material;
+            State = state;
+            _texture = texture;
+            _solid = solid;
+        }
+
+        public void UpdateBurning(float flammability, int material, FireState state)
+        {
+            Flammability = flammability;
+            Material = material;
+            State = state;
+        }
+    }
+}
